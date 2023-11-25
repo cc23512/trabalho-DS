132d781e0880e8caddb2adda334b2ea81d1f0ecc
cc23512 <142462802+cc23512@users.noreply.github.com>
2023-11-24 21:20:41 -0300

    atualização express

--- src/config/express.js
+++ src/config/express.js
@@ -4,12 +4,11 @@ const moment = require("moment");
 const app = express();
 
 app.use(session({
-    secret: 'BD23512', // Substitua com uma string secreta mais segura
+    secret: 'BD23512', 
     resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } // Defina para true se estiver usando HTTPS
+    saveUninitialized: 'true',
 }));
-// ...
+
 app.use(express.static('public'));
 app.use(express.static('views'))
 
@@ -20,8 +19,6 @@ app.set("view engine", "ejs");
 app.use(expressLayout);
 
 app.locals.moment = moment;
-// ...
-
 
 
 const rotas = require("../app/ROTAS/rotas");
